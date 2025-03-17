647fe91f29efe3033123c8d6661dbc72f716e1b6
이정훈 <eu4ng97@gmail.com>
2025-03-17 20:24:26 +0900

    Fix: DeleteData 메서드를 런타임에서 호출 가능하도록 변경 (#5)
    * Docs: README 수정
    
    * Fix: DataManager 클래스의 LoadData, DeleteData 메서드 오류 수정
    
    * Fix: DeleteAllSaveFiles를 에디터 전용 메서드로 변경 및 리팩토링

--- package/Runtime/DataManager.cs
+++ package/Runtime/DataManager.cs
@@ -179,23 +179,13 @@ namespace Eu4ng.Manager.Data
         {
             var dataType = typeof(T);
 
-            // 기존 데이터 로딩 작업이 존재하면 완료될 때까지 대기 및 완료 후 작업 목록에서 제거
-            if (m_LoadDataTasks.TryGetValue(dataType, out var task))
-            {
-                task.Wait();
-                m_LoadDataTasks.Remove(dataType);
-
-                return (T)m_LoadedData[dataType];
-            }
-
-            // 데이터 로딩 (캐시 확인 포함)
             var loadDataTask = LoadDataAsync<T>();
             loadDataTask.Wait();
+            m_LoadDataTasks.Remove(dataType);
 
             LogDataManager.Log(dataType.Name + " is loaded");
 
-            // 로드된 데이터 반환
-            return loadDataTask.Result;
+            return (T)m_LoadedData[dataType];
         }
 
         /// <summary>
@@ -260,15 +250,13 @@ namespace Eu4ng.Manager.Data
         {
             var dataType = typeof(T);
 
-            // 데이터 저장 요청 목록에 존재하는 경우 목록에서 제거 및 저장
-            if (m_DataToSave.Contains(dataType))
-            {
-                m_DataToSave.Remove(dataType);
-                SaveDataAsync(m_LoadedData[dataType]);
-            }
+            // 데이터 저장 요청 목록에 존재하는 경우 목록에서 제거
+            if (m_DataToSave.Contains(dataType)) m_DataToSave.Remove(dataType);
 
             // 데이터 언로드
             m_LoadedData.Remove(dataType);
+
+            LogDataManager.Log(dataType.Name + " is unloaded.");
         }
 
         /// <summary>
@@ -278,21 +266,25 @@ namespace Eu4ng.Manager.Data
         {
             var dataType = typeof(T);
 
+            // 데이터 언로드
+            UnloadData<T>();
+
             // 저장된 데이터 파일이 존재하는지 확인
             var path = Path.Combine(m_SaveFolderPath, GetFileName(dataType));
             if (!File.Exists(path)) return;
 
             // 데이터 파일 삭제
             File.Delete(path);
+
+            LogDataManager.Log(dataType.Name + " is deleted.");
         }
 
         /// <summary>
         /// 데이터 저장 폴더 비우기
         /// </summary>
 #if UNITY_EDITOR
-        [MenuItem("Tools/DataManager/Delete All")]
-#endif
-        public static void DeleteAll()
+        [MenuItem("Tools/DataManager/Delete All Save Files")]
+        public static void DeleteAllSaveFiles()
         {
             // 저장된 데이터 확인
             if (!Directory.Exists(m_SaveFolderPath)) return;
@@ -302,9 +294,12 @@ namespace Eu4ng.Manager.Data
             // 데이터 삭제
             foreach (var file in files)
             {
-                File.Delete(Path.Combine(m_SaveFolderPath, file));
+                File.Delete(file);
+
+                LogDataManager.Log(file + " is deleted.");
             }
         }
+#endif
 
         /* 메서드 */
         static string GetFileName(Type dataType) => dataType.Name + ".json";
